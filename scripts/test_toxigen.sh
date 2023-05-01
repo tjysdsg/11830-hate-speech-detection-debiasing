053eac5604ea6e4498ec7ef70d72c857262b0d74
tjysdsg <tjy1018543509@163.com>
2023-05-01 14:35:00 -0400

    Add scripts to test on Toxigen

---
+++ scripts/test_toxigen.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# test on Toxigen data
+checkpoint_dir=
+data_dir=data
+
+echo "Checkpoint dir: ${checkpoint_dir}"
+echo "Data dir: ${data_dir}"
+
+if [ -z "${checkpoint_dir}" ]; then
+  echo "Please specify checkpoint dir"
+  exit 2
+fi
+
+python run_model.py \
+  --do_eval \
+  --do_lower_case \
+  --data_dir ${data_dir} \
+  --bert_model NA \
+  --max_seq_length 128 \
+  --train_batch_size 32 \
+  --output_dir "${checkpoint_dir}" \
+  --task_name tox
