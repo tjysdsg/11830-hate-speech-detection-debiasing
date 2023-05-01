d04de050a0b793ad19c3808e7ac2d73bad02a31f
tjysdsg <tjy1018543509@163.com>
2023-05-01 13:45:34 -0400

    New training scripts

--- scripts/fou_soc.sh
+++ scripts/fou_soc.sh
@@ -1,40 +1,25 @@
 #!/bin/bash
 
-# training with regularzing SOC explanations
-
-if [[ -n "$1" ]]; then
-  current_seed=$1
-else
-  current_seed=0
-fi
-
-if [[ -n "$2" ]]; then
-  max_seeds=$2
-else
-  max_seeds=10
-fi
-
-current_seed=0
-
+# training with regularizing SOC explanations
+current_seed=42
 reg_strength=0.1
+data_dir=data
 
+echo "Data dir: ${data_dir}"
 echo "reg_strength is ${reg_strength}"
+
+soc_options="--hiex_add_itself --reg_explanations --nb_range 5 --sample_n 5 --negative_weight 0.1 --reg_strength ${reg_strength}"
 
 python run_model.py \
   --do_train --do_lower_case \
-  --data_dir ./data/ \
-  --bert_model bert-base-uncased \
+  --data_dir ${data_dir} \
+  --bert_model distilbert-base-uncased \
   --max_seq_length 128 \
   --train_batch_size 32 \
   --learning_rate 2e-5 \
-  --num_train_epochs 20 \
+  --num_train_epochs 5 \
   --early_stop 5 \
-  --output_dir runs/fou_soc_seed_${current_seed} \
+  --output_dir runs/founta_soc \
   --seed ${current_seed} \
   --task_name fou \
-  --hiex_add_itself \
-  --reg_explanations \
-  --nb_range 5 \
-  --sample_n 5 \
-  --negative_weight 0.1 \
-  --reg_strength ${reg_strength}
+  ${soc_options}
--- scripts/fou_vanilla.sh
+++ scripts/fou_vanilla.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
 # training on Founta data without regularization
-
-max_seeds=10
 current_seed=42
+data_dir=data
+echo "Data dir: ${data_dir}"
 
-# while (($current_seed < $max_seeds)); do
 python run_model.py \
   --do_train --do_lower_case \
-  --data_dir ./data/ \
-  --bert_model bert-base-uncased \
+  --data_dir ${data_dir} \
+  --bert_model distilbert-base-uncased \
   --max_seq_length 128 \
   --train_batch_size 32 \
   --learning_rate 2e-5 \
-  --num_train_epochs 20 \
+  --num_train_epochs 5 \
   --early_stop 5 \
-  --output_dir runs/fou_vanilla_seed_$current_seed \
+  --output_dir runs/founta_dbert \
   --seed $current_seed \
   --task_name fou \
   --negative_weight 0.1
-
-  # let current_seed++
