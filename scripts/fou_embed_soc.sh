2303298c9cdb1051510cd48d14c09f8b3e38314c
Jiyang Tang <jiyangta@andrew.cmu.edu>
2023-05-01 18:16:26 -0400

    Add founta embed soc exp

---
+++ scripts/fou_embed_soc.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# training with regularizing SOC explanations, using debiased distilbert
+current_seed=42
+reg_strength=0.1
+data_dir=data
+
+echo "Data dir: ${data_dir}"
+echo "reg_strength is ${reg_strength}"
+
+soc_options="--hiex_add_itself --reg_explanations --nb_range 5 --sample_n 5 --reg_strength ${reg_strength}"
+
+python run_model.py \
+  --do_train --do_lower_case \
+  --data_dir ${data_dir} \
+  --bert_model context-debias/debiased_models/42/dbert \
+  --max_seq_length 128 \
+  --train_batch_size 32 \
+  --learning_rate 2e-5 \
+  --num_train_epochs 3 \
+  --output_dir runs/founta_embed_dbert_soc \
+  --seed ${current_seed} \
+  --task_name fou \
+  ${soc_options}
